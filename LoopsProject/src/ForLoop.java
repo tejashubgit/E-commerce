362f34fa734d7de53b9aa644dbdeea22710eb378
Admin <Admin@DESKTOP-AHN7MSQ>
2020-10-18 10:46:59 +0530

    loops committed

---
+++ LoopsProject/src/ForLoop.java
@@ -0,0 +1,14 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+for(int i=10; i<20; i++)
+{
+	System.out.println(i);
+}
+
+System.out.println("Out Of Loop");
+	}
+
+}
---
+++ LoopsProject/src/WhileLoop.java
@@ -0,0 +1,19 @@
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		
+		while(a<=20)
+			
+		{
+			System.out.println(a);
+			a++;
+		}
+		
+		System.out.println("out of loop");
+
+	}
+
+}
